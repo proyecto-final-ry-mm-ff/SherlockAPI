8b47bfa2c659d4adcc8403e132a79627826c7384
Felipe <60165757+felipe-feijoo@users.noreply.github.com>
2024-10-26 20:49:25 -0300

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace SherlockAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "history")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
